98c11841a07af3c2e7642b03add3f64567e443ea
Cândido Silva <candido.da.silva.ca@gmail.com>
2024-07-15 16:09:18 +0100

    Day 15-07-2024, foi adicionado a relação entre tabela User e Story

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function stories()
+    {
+        return $this->hasMany(Story::class);
+    }
 }
