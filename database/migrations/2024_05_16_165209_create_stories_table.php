76fe38fb63e05bdac66842ce711cb463c9164371
Cândido Silva <candido.da.silva.ca@gmail.com>
2024-07-16 21:10:48 +0100

    Day 16-07-2024, Tive quen alterar ForeigIdFor para ForeignId divido ao constragimento.

--- database/migrations/2024_05_16_165209_create_stories_table.php
+++ database/migrations/2024_05_16_165209_create_stories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('stories', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor('user_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->string('title');
             $table->string('subtitle');
             $table->text('summary');
