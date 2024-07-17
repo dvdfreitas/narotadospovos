0438ead4cb38fad3ab64105c920d630bd8e09614
Cândido Silva <candido.da.silva.ca@gmail.com>
2024-07-17 17:42:31 +0100

    Day 17-07-2024, Acrescentei o campo 'slug' a tabela

--- database/migrations/2024_07_15_113937_create_categories_table.php
+++ database/migrations/2024_07_15_113937_create_categories_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('slug')->unique();
             $table->string('name');
             $table->string('description')->nullable();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
 
