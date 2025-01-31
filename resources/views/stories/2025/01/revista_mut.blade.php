9664568c9aaa1c499690855375ee4515560b5142
dvdfreitas <dvdfreitas@gmail.com>
2025-01-31 22:46:21 +0000

    250131

--- resources/views/stories/2025/01/revista_mut.blade.php
+++ resources/views/stories/2025/01/revista_mut.blade.php
@@ -10,7 +10,7 @@
         <p>No dia 24 de maio de 2024, a Na Rota Dos Povos recebeu o Prémio Mutualismo e Solidariedade Internacional da <a class="text-nrp-blue" href="https://mutualismo.pt/">União das Mutualidades Portuguesas</a> pelo reconhecimento do seu papel na promoção do desenvolvimento, educação, solidariedade social e saúde na Guiné-Bissau, com base no voluntariado.</p>
         <p>Hoje temos a honra de aparecer na revista <a class="text-nrp-blue" href="https://mutualismo.pt/anexos/RevistaMUT25.pdf">Mut - A Revista do Mutualismo</a>, onde partilhámos um pouco da nossa história e do nosso trabalho.</p>
         <p class="text-sm">Pode ver a revista completa clicando na imagem abaixo.</p>
-        <a href="https://mutualismo.pt/anexos/RevistaMUT25.pdf"><img class="max-w-xl rounded m-auto my-12" src="/stories/2025/01/revista_mut/cover.jpg" class="w-full h-auto mb-4" alt="Mut - A Revista do Mutualismo"></a>
+        <a class="max-w-xl" href="https://mutualismo.pt/anexos/RevistaMUT25.pdf"><img class="max-w-xl rounded m-auto my-12" src="/stories/2025/01/revista_mut/cover.jpg"  alt="Mut - A Revista do Mutualismo"></a>
         <div class="grid grid-cols-1 lg:grid-cols-2">
             <img class="rounded m-auto my-12" src="/stories/2025/01/revista_mut/page1.jpg" alt="Mut - A Revista do Mutualismo">
             <img class="rounded m-auto my-12" src="/stories/2025/01/revista_mut/page2.jpg" alt="Mut - A Revista do Mutualismo">
